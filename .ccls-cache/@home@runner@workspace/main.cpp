843c969faaaedddc2616e8c8ebd77391930a4ed1
MrLuca444 <cog2808@email.vccs.edu>
2025-05-07 16:25:02 +0000

    udhfoijah

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
+using namespace std;
+
+// Function prototypes
+double findLowest(double a, double b, double c, double d, double e);
+double findHighest(double a, double b, double c, double d, double e);
+
 int main() {
-int f = 0;
-  std::cout << "F|C" << std::endl;
-  std::cout << "---" << std::endl;
-  for (; f < 21;){
-    int c = static_cast<int>((5.0/9.0) * (f-32));
-    std::cout << f << "|" << c << std::endl;
-    f++;
-  }
-  
+    double s1, s2, s3, s4, s5;
+    cout << "Enter the five scores separated by spaces:" << endl;
+    cin >> s1 >> s2 >> s3 >> s4 >> s5;
+
+    double lowest = findLowest(s1, s2, s3, s4, s5);
+    double highest = findHighest(s1, s2, s3, s4, s5);
+    double average = (s1 + s2 + s3 + s4 + s5 - lowest - highest) / 3;
+
+    cout << "Your score is " << average << endl;
+
+    return 0;
+}
+
+double findLowest(double a, double b, double c, double d, double e) {
+    double min = a;
+    if (b < min) min = b;
+    if (c < min) min = c;
+    if (d < min) min = d;
+    if (e < min) min = e;
+    return min;
+}
+
+double findHighest(double a, double b, double c, double d, double e) {
+    double max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    if (d > max) max = d;
+    if (e > max) max = e;
+    return max;
 }
